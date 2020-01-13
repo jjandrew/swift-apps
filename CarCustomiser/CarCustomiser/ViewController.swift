bce5712272023033157e9732c653dfb233824f7e
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-01-13 13:03:56 +0000

    very strange logical error on last two switches even though they are identical to other two

--- CarCustomiser/CarCustomiser/ViewController.swift
+++ CarCustomiser/CarCustomiser/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var carStatistics: UILabel!
+    @IBOutlet var engineAndExhaustPackage: UISwitch!
+    @IBOutlet var tiresPackage: UISwitch!
+    @IBOutlet var brakesPackage: UISwitch!
+    @IBOutlet var suspensionPackage: UISwitch!
     
+    @IBOutlet var carStatistics: UILabel!
+    @IBOutlet var remainingFundsDisplay: UILabel!
+    
+    var remainingFunds = 800 {
+        didSet {
+            remainingFundsDisplay.text = "Remaining Funds: \(remainingFunds)"
+        }
+    }
     var carNumber = 0
     
     var StarterCars = starterCars()
@@ -25,7 +36,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        car = StarterCars.cars[carNumber]
+        car = StarterCars.cars[0]
+        remainingFundsDisplay.text = "Remaining Funds: \(remainingFunds)"
     }
 
     @IBAction func nextCar(_ sender: Any) {
@@ -37,6 +49,80 @@ class ViewController: UIViewController {
         car = StarterCars.cars[carNumber]
     }
 
+    func checkPrices() {
+        if remainingFunds < 500 {
+            engineAndExhaustPackage.isEnabled = false
+            tiresPackage.isEnabled = false
+            if remainingFunds < 250 {
+                brakesPackage.isEnabled = false
+                suspensionPackage.isEnabled = false
+            } else {
+                brakesPackage.isEnabled = true
+                suspensionPackage.isEnabled = true
+            }
+        } else {
+            engineAndExhaustPackage.isEnabled = true
+            tiresPackage.isEnabled = true
+            brakesPackage.isEnabled = true
+            suspensionPackage.isEnabled = true
+        }
+        if engineAndExhaustPackage.isOn {
+            engineAndExhaustPackage.isEnabled = true
+        }
+        if tiresPackage.isOn {
+            tiresPackage.isEnabled = true
+        }
+        if brakesPackage.isOn {
+            brakesPackage.isEnabled = true
+        }
+        if suspensionPackage.isOn {
+            suspensionPackage.isEnabled = true
+        }
+    }
+    
+    @IBAction func engineAndExhaustToggle(_ sender: Any) {
+        if engineAndExhaustPackage.isOn {
+            car?.topSpeed += 5
+            remainingFunds -= 500
+        } else {
+            car?.topSpeed -= 5
+            remainingFunds += 500
+        }
+        checkPrices()
+    }
+    
+    @IBAction func tiresPackageToggle(_ sender: Any) {
+        if tiresPackage.isOn {
+            car?.handling += 2
+            remainingFunds -= 500
+        } else {
+            car?.handling -= 2
+            remainingFunds += 500
+        }
+        checkPrices()
+    }
 
+    @IBAction func brakesPackageToggle(_ sender: Any) {
+        if brakesPackage.isOn {
+            car?.handling += 1
+            remainingFunds -= 250
+        } else {
+            car?.handling -= 1
+            remainingFunds += 250
+        }
+        checkPrices()
+    }
+    
+    @IBAction func suspensionPackageToggle(_ sender: Any) {
+        if suspensionPackage.isOn {
+            car?.acceleration += 1.0
+            remainingFunds -= 250
+        } else {
+            car?.acceleration -= 1.0
+            remainingFunds += 250
+        }
+        checkPrices()
+    }
+    
+    
 }
-
