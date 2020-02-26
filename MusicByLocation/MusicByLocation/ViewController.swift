3734db69394cf4a670fea603953d65166ea5e331
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-02-26 19:03:46 +0000

    finished video 3

--- MusicByLocation/MusicByLocation/ViewController.swift
+++ MusicByLocation/MusicByLocation/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    let geocoder = CLGeocoder()
+    
+    @IBOutlet var musicRecommendations: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
     }
 
-
+    @IBAction func findMusic(_ sender: Any) {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let firstLocation = locations.first {
+            geocoder.reverseGeocodeLocation(firstLocation, completionHandler: { (placemarks, error) in
+                if error != nil {
+                    self.musicRecommendations.text = "Could not perform lookup of location for latitude: \(firstLocation.coordinate.latitude.description)"
+                } else {
+                    self.musicRecommendations.text = placemarks?[0].locality
+                }
+                //build up a nice display and break down of the users location.
+            })
+            musicRecommendations.text = firstLocation.coordinate.latitude.description
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        musicRecommendations.text = "Could not access user's location. Error: \(error.localizedDescription)"
+    }
 }
 
