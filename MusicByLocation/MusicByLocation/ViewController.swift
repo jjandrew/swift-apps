e88f8dfc8d653de8d7701236463c8801b8e5d6cd
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-03-05 17:31:32 +0000

    Finished videos requires a debug

--- MusicByLocation/MusicByLocation/ViewController.swift
+++ MusicByLocation/MusicByLocation/ViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 import CoreLocation
 
-// outlet, action, requesting location, acting as location manager delegate, making geocoder lookups, tweaking response to get artist names., updating display
+// outlet, action, tweaking response to get artist names., updating display
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager()
-    let geocoder = CLGeocoder()
     let iTunesAdaptor = ITunesAdaptor()
+    let locationManagerDelegate = LocationManagerDelegate()
     
     var streetName: String? = nil
     var county: String? = ""
@@ -28,7 +28,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
-        locationManager.delegate = self
+        locationManager.delegate = locationManagerDelegate
+        locationManagerDelegate.vc = self
         locationManager.requestLocation()
     }
 
@@ -36,63 +37,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestLocation()
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let firstLocation = locations.first {
-            geocoder.reverseGeocodeLocation(firstLocation, completionHandler: { (placemarks, error) in
-                if error != nil {
-                    self.musicRecommendations.text = "Could not perform lookup of location for latitude: \(firstLocation.coordinate.latitude.description)"
-                } else {
-                    if let firstPlacemark = placemarks?[0] {
-                        self.iTunesAdaptor.getArtists(search: firstPlacemark.locality) { (artists) in
-                            let names = artists?.map { return $0.artistName }
-                            self.musicRecommendations.text = names?.joined(separator: ", " )
-                            
-                        }
-                        self.houseName = placemarks?[0].name
-                        if self.houseName == nil {
-                            self.houseName = "No house name available"
-                        } else {
-                            self.houseName = self.houseName!
-                        }
-                        self.streetName = placemarks?[0].thoroughfare
-                        if self.streetName == nil {
-                            self.streetName = "No street name available"
-                        } else {
-                            self.streetName = self.streetName!
-                        }
-                        self.county = placemarks?[0].locality
-                        if self.county == nil {
-                            self.county = "No county available"
-                        } else {
-                            self.county = self.county!
-                        }
-                        self.country = placemarks?[0].country
-                        if self.country == nil {
-                            self.country = "No country available"
-                        } else {
-                            self.country = self.country!
-                        }
-                        self.postCode = placemarks?[0].postalCode
-                        if self.postCode == nil {
-                            self.postCode = "No postal code available"
-                        } else {
-                            self.postCode = self.postCode!
-                        }
-                        self.musicRecommendations.text = """
-                        \(self.houseName!)
-                        \(self.streetName!)
-                        \(self.county!)
-                        \(self.country!)
-                        \(self.postCode!)
-                        """
-                    }
-                }
-            })
-        }
+    func updateDisplay(text: String?) {
+        musicRecommendations.text = text
     }
     
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    
+    func updateArtistsByLocation(text: String?) {
+        iTunesAdaptor.getArtists(search: text) { (artists) in
+            let names = artists?.map { return $0.artistName }
+            self.musicRecommendations.text = names?.joined(separator: ", " )
+        }
     }
     
 }
