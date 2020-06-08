16878b4a78d4868244801a3b976ab800aa77d740
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-06-08 12:10:54 +0100

    connected buttons

--- Reverse Polish notation/Reverse Polish notation/ViewController.swift
+++ Reverse Polish notation/Reverse Polish notation/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var expressionDisplay: UILabel!
+    @IBOutlet var zeroButton: UIButton!
+    @IBOutlet var oneButton: UIButton!
+    @IBOutlet var twoButton: UIButton!
+    @IBOutlet var threeButton: UIButton!
+    @IBOutlet var fourButton: UIButton!
+    @IBOutlet var fiveButton: UIButton!
+    @IBOutlet var sixButton: UIButton!
+    @IBOutlet var sevenButton: UIButton!
+    @IBOutlet var eightButton: UIButton!
+    @IBOutlet var nineButton: UIButton!
+    @IBOutlet var evaluateButton: UIButton!
+    @IBOutlet var clearButton: UIButton!
+    @IBOutlet var enterButton: UIButton!
+    @IBOutlet var negateButton: UIButton!
+    @IBOutlet var plusButton: UIButton!
+    @IBOutlet var minusButton: UIButton!
+    @IBOutlet var multiplyButton: UIButton!
+    @IBOutlet var divideButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
