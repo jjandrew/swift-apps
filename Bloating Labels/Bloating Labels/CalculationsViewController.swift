55e0803eed57ad84d09554daa266a69c2ffe827a
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-06-24 10:23:39 +0100

    view controllers

--- Bloating Labels/Bloating Labels/CalculationsViewController.swift
+++ Bloating Labels/Bloating Labels/CalculationsViewController.swift
@@ -2,7 +2,7 @@
 //  CalculationsViewController.swift
 //  Bloating Labels
 //
-//  Created by JJ Andrew on 23/06/2020.
+//  Created by JJ Andrew on 24/06/2020.
 //  Copyright © 2020 JJ Andrew. All rights reserved.
 //
 
@@ -12,8 +12,19 @@ class CalculationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
--- Bloating Labels/Bloating Labels/ProfileViewController.swift
+++ Bloating Labels/Bloating Labels/ProfileViewController.swift
@@ -2,18 +2,45 @@
 //  ProfileViewController.swift
 //  Bloating Labels
 //
-//  Created by JJ Andrew on 23/06/2020.
+//  Created by JJ Andrew on 24/06/2020.
 //  Copyright © 2020 JJ Andrew. All rights reserved.
 //
 
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet var totalItems: UILabel!
+    @IBOutlet var totalProfit: UILabel!
+    @IBOutlet var totalValueStock: UILabel!
+    @IBOutlet var potentialProfit: UILabel!
+    @IBOutlet var amountSpent: UILabel!
+    @IBOutlet var amountSold: UILabel!
+    @IBOutlet var numItemsSold: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        totalItems.text = "14"
+        totalProfit.text = "-9.82"
+        totalValueStock.text = "395"
+        potentialProfit.text = "344.71"
+        amountSpent.text = "50.31"
+        amountSold.text = "40.47"
+        numItemsSold.text = "5"
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
