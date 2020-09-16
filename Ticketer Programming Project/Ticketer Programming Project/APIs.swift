c2d3a4aeadb25b5c6a2968d75f1674ae6552c4e8
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-09-16 13:19:17 +0100

    Done some more UI

--- Ticketer Programming Project/Ticketer Programming Project/APIs.swift
+++ Ticketer Programming Project/Ticketer Programming Project/APIs.swift
@@ -9,10 +9,13 @@
 import Foundation
 
 // Skiddle API key: 52f07458a25911d3e1b89ab9f7f0c448
+// https://github.com/Skiddle/web-api
 
 // TicketMaster: https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/
+// 0URy7rbQGi9UscAHL7dnRQhn1Wol9hwi
 
 // Event Brite API key: 6SBP6EZOTOLK44UP66MT
+// https://www.eventbrite.com/platform/api#/introduction/authentication/2.-(for-app-partners)-authorize-your-users
 
 // Stubhub: https://developer.stubhub.com/products/catalog
 
