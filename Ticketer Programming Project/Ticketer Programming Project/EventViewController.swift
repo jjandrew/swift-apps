0d794032c2f4764d67f31ab48e803edc37b41f84
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-10-04 14:10:41 +0100

    hooker up event view labels

---
+++ Ticketer Programming Project/Ticketer Programming Project/EventViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EventViewController.swift
+//  Ticketer Programming Project
+//
+//  Created by JJ Andrew on 04/10/2020.
+//  Copyright © 2020 JJ Andrew. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    @IBOutlet var savedButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var venueLabel: UILabel!
+    @IBOutlet var distanceLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var demographicLabel: UILabel!
+    @IBOutlet var purchaseButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
