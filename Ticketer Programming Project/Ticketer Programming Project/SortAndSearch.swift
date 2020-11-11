aa8ed0aaf429f577786d99986f109c75638900ac
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-11-11 16:15:03 +0000

    sort by date

--- Ticketer Programming Project/Ticketer Programming Project/SortAndSearch.swift
+++ Ticketer Programming Project/Ticketer Programming Project/SortAndSearch.swift
@@ -48,13 +48,86 @@ class SortAndSearch {
             }
             let leftArray = mergeSortByName(array: left)
             let rightArray = mergeSortByName(array: right)
-            return merge(left: leftArray, right: rightArray)
+            return mergeByName(left: leftArray, right: rightArray)
         } else {
             return array
         }
     }
 
-    func merge(left: [Event], right: [Event]) -> [Event] {
+    func mergeByName(left: [Event], right: [Event]) -> [Event] {
+        var leftArray = left
+        var rightArray = right
+        var mergedArray: [Event] = []
+        while leftArray.count > 0 && rightArray.count > 0 {
+            if leftArray[0].eventName < rightArray[0].eventName {
+                mergedArray.append(leftArray[0])
+                leftArray.removeFirst()
+            } else {
+                mergedArray.append(rightArray[0])
+                rightArray.removeFirst()
+            }
+        }
+        
+        while leftArray.count > 0 {
+            mergedArray.append(leftArray[0])
+            leftArray.removeFirst()
+        }
+        
+        while rightArray.count > 0 {
+            mergedArray.append(rightArray[0])
+            rightArray.removeFirst()
+        }
+        return mergedArray
+    }
+    
+    func quickSortByDate(array: [Event]) -> [Event] {
+      var left = [Event]()
+      var right = [Event]()
+      var newData = [Event]()
+      if array.count < 2 {
+        return array
+      } else {
+        let pivot = array[0]
+        let pivotDateArray = pivot.date.split(separator: "/")
+        let pivotDateInt = Int(pivotDateArray[2])!+Int(pivotDateArray[1])!+Int(pivotDateArray[0])!
+        for i in 1..<array.count {
+            let item = array[i]
+            let itemDateArray = item.date.split(separator: "/")
+            let itemDateInt = Int(itemDateArray[2])!+Int(itemDateArray[1])!+Int(itemDateArray[0])!
+            //error is going to be should've kept as string
+            if itemDateInt < pivotDateInt {
+                left.append(item)
+             } else {
+               right.append(item)
+             }
+       }
+      newData.append(contentsOf: (quickSortByName(array: left)))
+      newData.append(pivot)
+      newData.append(contentsOf: (quickSortByName(array: right)))
+      return newData
+      }
+    }
+    
+    func mergeSortByDate(array: [Event]) -> [Event] {
+        if array.count > 1 {
+            let mid = array.count / 2
+            var left: [Event] = []
+            var right: [Event] = []
+            for i in 0..<mid {
+                left.append(array[i])
+            }
+            for i in mid..<array.count {
+                right.append(array[i])
+            }
+            let leftArray = mergeSortByName(array: left)
+            let rightArray = mergeSortByName(array: right)
+            return mergeByDate(left: leftArray, right: rightArray)
+        } else {
+            return array
+        }
+    }
+
+    func mergeByDate(left: [Event], right: [Event]) -> [Event] {
         var leftArray = left
         var rightArray = right
         var mergedArray: [Event] = []
