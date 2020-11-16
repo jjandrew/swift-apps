50dfde176ac4e1d9a339727cb8d085c9cf3ca415
JJ Andrew <andrew.j@etoncollege.org.uk>
2020-11-16 21:35:26 +0000

    tied up profile edit

--- Ticketer Programming Project/Ticketer Programming Project/ViewControllers/Profile/EditProfileViewController.swift
+++ Ticketer Programming Project/Ticketer Programming Project/ViewControllers/Profile/EditProfileViewController.swift
@@ -10,21 +10,24 @@ import UIKit
 
 class EditProfileViewController: UIViewController {
 
+    @IBOutlet var nameEntryTextField: UITextField!
+    @IBOutlet var dateOfBirthPicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func confirmNameEntryButton(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func maleButton(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func femaleButton(_ sender: Any) {
+    }
+    
+    @IBAction func otherButton(_ sender: Any) {
+    }
 }
